6bd0e6e8cf0a0c734285af3e88e0aba10c793609
Travis Sigler <travis.c.sigler@gmail.com>
2021-02-02 18:23:04 -0500

    Converted player data to new PlayerModel class
    Purpose: This logically groups related data together
    Notes:
    - All references to old vars updated
    - Unit testing successful

---
+++ csharp/Tennis/PlayerModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tennis
+{
+    public class PlayerModel
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
--- csharp/Tennis/TennisGame.cs
+++ csharp/Tennis/TennisGame.cs
@@ -2,32 +2,30 @@ namespace Tennis
 {
     class TennisGame : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        PlayerModel Player1 = new PlayerModel();
+        PlayerModel Player2 = new PlayerModel();
 
         public TennisGame(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            Player1.Name = player1Name;
+            Player2.Name = player2Name;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                Player1.Score += 1;
             else
-                m_score2 += 1;
+                Player2.Score += 1;
         }
 
         public string GetScore()
         {
             string score = "";
             var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (Player1.Score == Player2.Score)
             {
-                switch (m_score1)
+                switch (Player1.Score)
                 {
                     case 0:
                         score = "Love-All";
@@ -44,9 +42,9 @@ namespace Tennis
 
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (Player1.Score >= 4 || Player2.Score >= 4)
             {
-                var minusResult = m_score1 - m_score2;
+                var minusResult = Player1.Score - Player2.Score;
                 if (minusResult == 1) score = "Advantage player1";
                 else if (minusResult == -1) score = "Advantage player2";
                 else if (minusResult >= 2) score = "Win for player1";
@@ -56,8 +54,8 @@ namespace Tennis
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    if (i == 1) tempScore = Player1.Score;
+                    else { score += "-"; tempScore = Player2.Score; }
                     switch (tempScore)
                     {
                         case 0:
